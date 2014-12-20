7ad196cea92cc78ca50768041a3c3e2a83420324
Andreas Hermann <a.v.hermann@gmail.com>
2014-12-20 20:42:11 +0100

    Implementation of makeCacheMatirx and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions "makeCacheMatrix" and "cacheSolve" can be used together to
+## avoid the repeated invocation of the time consuming "solve" operation.
+## e.g. 
+## m <- makeCacheMatrix(matrix(rnorm(9), 3, 3))
+## m.1 <- cacheSolve(m)
 
-## Write a short comment describing this function
+## Creates a wrapper around a matrix that can store
+## the result of a time consuming operation.
+## The input matrix can be read and modified with the $get and $set functions
+## The cached results can be accessed with $get.cache and set with $set.cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  get <- function() x
+  
+  set.cache <- function(data) cache <<- data
+  get.cache <- function() cache
+  list(set = set, get = get,
+       set.cache = set.cache,
+       get.cache = get.cache)
 }
 
 
-## Write a short comment describing this function
+## Solves the inverse of a matrix that has been wrapped
+## with the function "makeCacheMatrix".
+## The result of the solve function is returned and stored in the given matrix.
+## The same result is returned on sub sequent invocations of this function.
+## All additional parameters are passed to the solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get.cache()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  inv <- solve(x$get(), ...)
+  x$set.cache(inv)
+  inv
 }
